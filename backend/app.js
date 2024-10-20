10c646fca2f6efd1b216958f496822b003127ab5
Andrew Lambert <hg9958@wayne.edu>
2024-10-20 18:21:49 -0400

    Updating database changes

--- backend/app.js
+++ backend/app.js
@@ -1,148 +1,107 @@
 // Backend: application services, accessible by URIs
 
-const express = require('express')
-const cors = require ('cors')
-const dotenv = require('dotenv')
-dotenv.config()
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
-const dbService = require('./dbService');
-
+const dbService = require('./dbService');  // DbService is assumed to be the modified dbService.js
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-// create
-app.post('/insert', (request, response) => {
-    console.log("app: insert a row.");
-    // console.log(request.body); 
-
-    const {name} = request.body;
+// CREATE: Insert a new user
+app.post('/insertUser', (request, response) => {
+    console.log("app: insert a user.");
+    
+    const { username, password, firstname, lastname, age, salary } = request.body;
     const db = dbService.getDbServiceInstance();
 
-    const result = db.insertNewName(name);
- 
+    const registerday = new Date().toISOString().split('T')[0];  // current date
+    const signintime = new Date().toISOString().replace('T', ' ').split('.')[0];  // current datetime
+
+    const result = db.insertNewUser(username, password, firstname, lastname, age, salary, registerday, signintime);
+
     // note that result is a promise
     result 
-    .then(data => response.json({data: data})) // return the newly added row to frontend, which will show it
-   // .then(data => console.log({data: data})) // debug first before return by response
-   .catch(err => console.log(err));
+    .then(data => response.json({data: data})) // Return the newly added user to frontend
+    .catch(err => console.log(err));
 });
 
-
-
-
-// read 
-app.get('/getAll', (request, response) => {
-    console.log('test');
+// READ: Get all users
+app.get('/getAllUsers', (request, response) => {
+    console.log('Fetching all users');
     const db = dbService.getDbServiceInstance();
 
-    
-    const result =  db.getAllData(); // call a DB function
+    const result = db.getAllData(); // call a DB function
 
     result
     .then(data => response.json({data: data}))
     .catch(err => console.log(err));
 });
 
-
-app.get('/search/:name', (request, response) => { // we can debug by URL
-    
-    const {name} = request.params;
-    
-    console.log(name);
+// READ: Search for a user by username
+app.get('/searchUser/:username', (request, response) => { 
+    const { username } = request.params;
+    console.log(`Searching for user: ${username}`);
 
     const db = dbService.getDbServiceInstance();
-
-    let result;
-    if(name === "all") // in case we want to search all
-       result = db.getAllData()
-    else 
-       result =  db.searchByName(name); // call a DB function
+    const result = db.searchByUsername(username); // call the appropriate DB function
 
     result
     .then(data => response.json({data: data}))
     .catch(err => console.log(err));
 });
 
+// UPDATE: Update user data by ID
+app.patch('/updateUser', (request, response) => {
+    console.log("app: update is called");
 
-// update
-app.patch('/update', 
-     (request, response) => {
-          console.log("app: update is called");
-          //console.log(request.body);
-          const{id, name} = request.body;
-          console.log(id);
-          console.log(name);
-          const db = dbService.getDbServiceInstance();
+    const { id, firstname, lastname } = request.body;
+    const db = dbService.getDbServiceInstance();
 
-          const result = db.updateNameById(id, name);
+    const result = db.updateUserById(id, firstname, lastname);
 
-          result.then(data => response.json({success: true}))
-          .catch(err => console.log(err)); 
-
-     }
-);
-
-// delete service
-app.delete('/delete/:id', 
-     (request, response) => {     
-        const {id} = request.params;
-        console.log("delete");
-        console.log(id);
-        const db = dbService.getDbServiceInstance();
-
-        const result = db.deleteRowById(id);
-
-        result.then(data => response.json({success: true}))
-        .catch(err => console.log(err));
-     }
-)   
-
-// debug function, will be deleted later
-app.post('/insert', (request, response) => {
-
+    result.then(data => response.json({success: true}))
+    .catch(err => console.log(err));
 });
 
+// DELETE: Delete user by ID
+app.delete('/deleteUser/:id', (request, response) => {
+    const { id } = request.params;
+    console.log(`Deleting user with ID: ${id}`);
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.deleteUserById(id);
+
+    result.then(data => response.json({success: true}))
+    .catch(err => console.log(err));
+});
+
+// Debug endpoint (optional, remove this in production)
 app.post('/debug', (request, response) => {
-    // console.log(request.body); 
-
-    const {debug} = request.body;
+    const { debug } = request.body;
     console.log(debug);
-
     return response.json({success: true});
-});   
+});
 
-// debug function: use http://localhost:5050/testdb to try a DB function
-// should be deleted finally
+// Debug function: You can use this to test any DB function temporarily
 app.get('/testdb', (request, response) => {
-    
     const db = dbService.getDbServiceInstance();
-
-    
-    const result =  db.deleteById("14"); // call a DB function here, change it to the one you want
-
+    const result = db.deleteById("14"); // call a DB function here for testing
     result
     .then(data => response.json({data: data}))
     .catch(err => console.log(err));
 });
 
+// Set up the web server listener
+app.listen(5050, () => {
+    console.log("I am listening on port 5050.")
+});
 
-// set up the web server listener
-// if we use .env to configure
-/*
-app.listen(process.env.PORT, 
-    () => {
-        console.log("I am listening on the configured port " + process.env.PORT)
-    }
-);
-*/
-
-// if we configure here directly
-app.listen(5050, 
-    () => {
-        console.log("I am listening on the fixed port 5050.")
-    }
-);
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+});
